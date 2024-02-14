59ea90bfc556ac0f0de8a550fa82822bbba51504
yasha <yashodhanpawar@gmail.com>
2024-02-14 21:53:05 +0530

    Findings for cutting rod problems

--- src/com/hiru/core_dsa_problems/algorithms/dynamicprogramming/CuttingRodToGetMaxValue.java
+++ src/com/hiru/core_dsa_problems/algorithms/dynamicprogramming/CuttingRodToGetMaxValue.java
@@ -12,9 +12,13 @@ import java.util.Arrays;
  * Learnings ==> Did not consider repeated items( like coin change), silly mistakes , Did not consider the permutations optimization
  * e.g. (1111112, 2111111)
  * <p>
+ * Findings ==> Geeks solution is different than this..It is same as knapsack ? with 2 choices at every stage.
+ * <p>
  * https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
  */
 public class CuttingRodToGetMaxValue {
+
+    private int numberOfCalls;
 
     ////////////////////////////////////////////
     public static void main(String[] args) {
@@ -27,11 +31,19 @@ public class CuttingRodToGetMaxValue {
     public int getMaxProfitCut(int[] rates) {
         int[] memo = new int[rates.length];
         Arrays.fill(memo, -1);
-        return getMaxProfitCutUtil(rates, rates.length, memo);
+        int maxProfit = getMaxProfitCutUtil(rates, rates.length, memo);
+        System.out.println("Number of calls: " + numberOfCalls);
+        return maxProfit;
     }
 
     ///////////////////////////////////////////////
     public int getMaxProfitCutUtil(final int[] rates, int capacity, int[] memo) {
+        // FINDING==> GFG number of calls 452 compared to our 256 (Even without memoization optimization)
+        // CONCLUSION ==>
+        //          Our approach ==> FOR loop for all items in each stage(recursive call) is WAY OPTIMAL :-)
+        //          GFG approach ==> Typical knapsack approach of 2 choices,consider/don't consider item.
+        //                              And reduce the capacity and decrement item index with base condition on index.
+        numberOfCalls += 1;
         // Base case: We have No more rod to sell ( this is the only base case, no other constraints)
         if (capacity == 0) {
             return 0;
@@ -44,10 +56,12 @@ public class CuttingRodToGetMaxValue {
         }
 
         int maxProfit = Integer.MIN_VALUE;
-        // LEARNINGS ==> Repeated items is allowed ( THEN DON'T consider it as knapsack problem)
-        // Consider it as coin change problem
-        // In each stage, consider ALL the possible choices and choose best among those
+
+        // CRITICAL LEARNINGS ==> Repeated items is allowed ( THEN DON'T consider it as knapsack problem)
+        // Consider it as coin change problem.
+
         // LEARNINGS ==> int arithmetic (capacity-rodPart+1) is different than (capacity-(rodPart+1))
+
         // LEARNINGS ==> Capacity being a reducing parameter and NO other constraints, we can simply have that as mutable/reducing
         // parameter and hence could have base case ONLY on top of that.
         for (int rodPart = 0; rodPart < rates.length; rodPart++) {
